d353d861aa0483c5c0aaae7887a4188ebb507e3d
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2024-02-16 01:52:14 +0100

    finalisation + commentaires pour apprentissage

--- Programs.dart
+++ Programs.dart
@@ -6,18 +6,32 @@ void main(List<String> arguments) {
     exit(1);
   }
   final inputFile = arguments.first;
-  // à recoder avec async / await syntaxe
+  // à recoder avec async / await syntaxe pour vérifier si inputFile != null
   final lines = File(inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double>{};
+  // retrait de la première ligne inutile
+  lines.removeAt(0);
+  var totalDuration = 0.0;
+  for (var line in lines) {
+    final values = line.split(',');
+    final durationString =
+        values[3].replaceAll('"', ''); //attention à la syntaxe: ' " ', ''
+    final duration = double.parse(
+        durationString); //memo : double.parse() = convertion d'une string en double
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null) {
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] = previousTotal + duration;
+    }
+    totalDuration += duration;
+  }
+  for (var entry in totalDurationByTag.entries) {
+    final durationFormated = entry.value.toStringAsFixed(1);
+    // entry.key == '' vérifie si entry.key n'existe pas (est égale un à string vide), la syntaxe !entry.key ne fonctionne pas en Dart
+    final tag = entry.key == '' ? 'Unallocated' : entry.key;
+    print('$tag: ${durationFormated}h');
+  }
+  print('Total for all tags: ${totalDuration.toStringAsFixed(1)}h');
 }
-/*
-lines = readFile(inputFile)
-durationByTag = empty tag
-lines.removeFirst()
-for (line in lines)
-  values = lines.split(',')
-  duration = values[3]
-  tag = values[5]
-  update(durationByTag[tag], duration)
-end
-printAll(durationByTag)
-*/
